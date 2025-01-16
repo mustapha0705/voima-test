7419518de2d7fdca9c9d481938776cc1cb5051eb
Mustapha Mohammed <mustaphamohammed0507@gmail.com>
2025-01-16 14:15:29 +0000

    added cors for hosted website

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://voima-dev.vercel.app"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
